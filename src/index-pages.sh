9b0d30029aa6880cdb2bf2ec045524108a684e07
Louie S <louie@example.com>
2024-07-17 18:07:15 -0400

    Set config.mk

--- src/index-pages.sh
+++ src/index-pages.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env sh
 
-# shellcheck source=./lib/create_table
-. "$(dirname "$0")"/lib/create_table
-# shellcheck source=./lib/insert
-. "$(dirname "$0")"/lib/insert
+# shellcheck source=../../../scripts/create_table.sh
+. "$(dirname "$0")"/../../../scripts/create_table.sh
+# shellcheck source=../../../scripts/get_title.sh
+. "$(dirname "$0")"/../../../scripts/get_title.sh
+# shellcheck source=../../../scripts/insert.sh
+. "$(dirname "$0")"/../../../scripts/insert.sh
 
 DB_PATH="$1"
 shift
-
-get_title() {
-	FILE="$1"
-
-	pup -p -f "$FILE" 'title text{}' | \
-		tr -d \\n | \
-		sed 's/\"/\"\"/g'
-}
 
 insert_pages() {
 	# Get title and insert into table for each html file
